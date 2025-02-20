310f9edd2ba2b31ecfd960ed8709032a332fc66f
zhang-ruijie-ai <zhang.ruijie.cn@gmail.com>
2025-02-20 11:43:02 +0800

    Update word_level_spacy.py

--- src/case_study/word_level_spacy.py
+++ src/case_study/word_level_spacy.py
@@ -1,32 +1,25 @@
 import os
 import json
-from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from tqdm import tqdm
 import math
 from scipy.stats import t
 import spacy
-
-# 加载英文模型
-nlp = spacy.load('en_core_web_sm')
-
 import string
-stop_words = set(stopwords.words('english'))
 
+nlp = spacy.load('en_core_web_sm')
+stop_words = set(stopwords.words('english'))
 text = "The quick brown fox jumps over the lazy dog"
 doc = nlp(text)
-
-# 输出每个单词的词性
 for token in doc:
     print(f"{token.text}: {token.pos_}")
 
-data_type = 'Abs'
+data_type = ''
 alpha=0.05
 alpha_smooth=1
 
-data_dir = '/mntcephfs/data/haizhouli/Lab-projects/lizhou/LLM_penetration/data'
-save_dir = '/mntcephfs/data/haizhouli/Lab-projects/lizhou/LLM_penetration/results/prefer_word'
+data_dir = ''
+save_dir = ''
 
 dataset_file = os.path.join(data_dir, 'ICLR_2017_2019_pol_seg.json')
 with open(dataset_file, "r", encoding="utf-8") as f:
@@ -62,33 +55,7 @@ compair_word_set = {
 }
 
 def map_pos(tag):
-    # 将细粒度的词性标签映射为粗粒度的标签，只保留名词、动词、形容词、副词、代词
-    pos_mapping = {
-        'NN': 'NOUN',
-        'NNS': 'NOUN',
-        'NNP': 'NOUN',
-        'NNPS': 'NOUN',
-        'VB': 'VERB',
-        'VBD': 'VERB',
-        'VBG': 'VERB',
-        'VBN': 'VERB',
-        'VBP': 'VERB',
-        'VBZ': 'VERB',
-        'JJ': 'ADJ',
-        'JJR': 'ADJ',
-        'JJS': 'ADJ',
-        'RB': 'ADV',
-        'RBR': 'ADV',
-        'RBS': 'ADV',
-        'PRP': 'PRONOUN',
-        'PRP$': 'PRONOUN',
-        'WP': 'PRONOUN',
-        'WP$': 'PRONOUN'
-    }
-    if tag in pos_mapping:
-        return pos_mapping[tag]
-    else:
-        return tag
+    return tag
 
 
 for d in tqdm(data):
@@ -96,14 +63,12 @@ for d in tqdm(data):
         human_written = d['abstract']
         gpt4_written = d['polish_abstract']
         claude_written = d['polish_abstract_claude']
-        gemini_written = d['polish_abstract_claude']
+        gemini_written = d['polish_abstract_gemini']
     else:
         human_written = d['meta_reivew']['comment']
         gpt4_written = d['meta_review_gpt']
         claude_written = d['meta_review_claude']
-        gemini_written = d['meta_review_claude']
-        ss = 'Our results demonstrate that both label smoothing and our confidence penalty enhance the performance of state-of-the-art models on these benchmarks without altering existing hyper-parameters.'
-        a=pos_tag(word_tokenize(ss))
+        gemini_written = d['meta_review_gemini']
         doc1 = [(token.text, token.pos_) for token in doc]
      
         
@@ -122,11 +87,7 @@ for d in tqdm(data):
     gemini_words = [(token.text.lower(), token.pos_) for token in nlp(gemini_written) if token.text.lower() not in stop_words and token.text not in string.punctuation]
     gemini_words_map = [(word[0], map_pos(word[1])) for word in gemini_words]
     All_word_per_text['gemini'].append([word for word in gemini_words_map])
-    
-    # human_words = [word.lower() for word in word_tokenize(human_written) if word.lower() not in stop_words and word not in string.punctuation]
-    # gpt4_words = [word.lower() for word in word_tokenize(gpt4_written) if word.lower() not in stop_words and word not in string.punctuation]
-    # claude_words = [word.lower() for word in word_tokenize(claude_written) if word.lower() not in stop_words and word not in string.punctuation]
-    # gemini_words = [word.lower() for word in word_tokenize(gemini_written) if word.lower() not in stop_words and word not in string.punctuation]
+
     All_word['human'].extend(human_words_map)
     All_word['gpt4'].extend(gpt4_words_map)
     All_word['claude'].extend(claude_words_map)
@@ -156,8 +117,6 @@ for pair, vocab in compair_word_set.items():
         cnt_human = sum([1 if v in per else 0 for per in All_word_per_text['human']]) + alpha_smooth
         cnt_llm = sum([1 if v in per else 0 for per in All_word_per_text[pair]]) + alpha_smooth
 
-        # cnt_human = All_word['human'].count(v) + alpha_smooth
-        # cnt_llm = All_word[pair].count(v) + alpha_smooth
         p_hat_human = cnt_human / all_cnt_human
         p_hat_llm = cnt_llm / all_cnt_llm
         
@@ -166,9 +125,8 @@ for pair, vocab in compair_word_set.items():
         se_human = math.sqrt(p_hat_human * (1 - p_hat_human) / all_cnt_human)
         se_llm = math.sqrt(p_hat_llm * (1 - p_hat_llm) / all_cnt_llm)
         se = math.sqrt(se_human ** 2 + se_llm ** 2)
-        # 计算 t 统计量
+
         t_stat = (p_hat_llm - p_hat_human) / se
-        # 计算自由度，使用 Welch's t-test 近似
 
         se_2_n_human =  (se_human ** 2) / all_cnt_human
         se_2_n_llm =  (se_llm ** 2) / all_cnt_llm
@@ -176,9 +134,9 @@ for pair, vocab in compair_word_set.items():
         df_parent = (se_2_n_human ** 2)/(all_cnt_human-1) + (se_2_n_llm ** 2)/(all_cnt_llm-1)
 
         df =  df_child / df_parent
-        # 计算 t 分布的临界值（右侧单侧检验）
+
         t_critical = t.ppf(1 - alpha, df)
-        # 判断是否拒绝原假设（只考虑 AI 显著高于人类）
+
         reject_null = t_stat > t_critical
         if reject_null:
             test_word[pair].append({
@@ -197,5 +155,3 @@ for pair, vocab in compair_word_set.items():
     with open(save_file, "w") as f:
         json.dump(test_word[pair], f, indent=4)
 
-
-print('a')
